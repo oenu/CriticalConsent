f565a086a985253d9b60fc7ba5c482db33517c66
oenu <51684443+oenu@users.noreply.github.com>
2022-09-20 14:54:12 -0400

    converted to switch

--- criticalConsent/src/features/question/QuestionList.tsx
+++ criticalConsent/src/features/question/QuestionList.tsx
@@ -1,66 +1,84 @@
 // Components
 import Question from "./Question";
-import { Button, Center, Container, Stack } from "@mantine/core";
-
-// Types
-import { QuestionType } from "../../types";
+import { Alert, Button, Center, Container, Stack } from "@mantine/core";
 
 // Redux
-import { selectAllQuestions, getQuestionsStatus } from "./questionSlice";
-import { useAppSelector } from "../../redux/hooks";
+import {
+  selectAllQuestions,
+  getQuestionsStatus,
+  uploadResponse,
+} from "./questionSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 
 function QuestionList() {
+  const dispatch = useAppDispatch();
   const questions = useAppSelector(selectAllQuestions);
   const questionsStatus = useAppSelector(getQuestionsStatus);
-
   const questionList = Object.values(questions);
 
-  let content: JSX.Element[] | JSX.Element | null = null;
-  if (questionsStatus === "loading") {
-    content = <div>Loading...</div>;
-  } else if (questionsStatus === "succeeded") {
-    if (questions === null) {
-      content = <div>No questions found</div>;
-    } else {
-      content = (
-        <Stack style={{ minWidth: "80vw" }}>
-          {questionList.map((question) => {
-            if (question !== null) {
-              return <Question key={question.id} question={question} />;
-            }
-          })}
+  // Warning Message if not all questions are answered
 
-          {/* Submit Button */}
-          <Center>
-            <Button
-              onClick={() => {
-                // Check if all questions have been answered and if so, submit
-                console.debug("submit");
-                if (
-                  questionList.every((question) => question?.answered === true)
-                ) {
-                  console.debug("all answered");
-                } else {
-                  console.debug("not all answered");
-                }
-              }}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Center>
-        </Stack>
+  const warningMessage = (
+    <Alert title="Oops!" color="red" hidden={false}>
+      Looks like you haven't answered all the questions yet. Please answer all
+      of them before continuing.
+    </Alert>
+  );
+
+  // Type definition for the question list content
+  let content: JSX.Element[] | JSX.Element | null = null;
+
+  switch (questionsStatus) {
+    // If the questions are loading, show a loading message
+    case "loading":
+      content = <div>Loading...</div>;
+      break;
+
+    // If there are no questions, show a message
+    case "succeeded":
+      if (questionList.length === 0) {
+        content = <div>There are no questions to show.</div>;
+      } else {
+        // If there are questions, show them
+        content = (
+          <Stack style={{ minWidth: "80vw" }}>
+            {questionList.map((question) => {
+              if (question !== null) {
+                return <Question key={question.id} question={question} />;
+              }
+            })}
+            <Center>{warningMessage}</Center>
+            {/* Submit Button */}
+            <Center>
+              <Button
+                onClick={() => {
+                  dispatch(uploadResponse());
+                }}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Center>
+          </Stack>
+        );
+      }
+      break;
+
+    // If there is an error, show a message
+    case "failed":
+      content = (
+        <div data-testid="errorMessage">
+          Something went wrong, please contact me on{" "}
+          <a href="https://twitter.com/_a_nb">twitter</a> or try again later.
+        </div>
       );
-    }
-  } else if (questionsStatus === "failed") {
-    content = (
-      <div data-testid="errorMessage">
-        Something went wrong, please contact me on{" "}
-        <a href="https://twitter.com/_a_nb">twitter</a> or try again later.
-      </div>
-    );
-  } else {
-    content = <div>No Question Status Provided</div>;
+      break;
+
+    // If something else happens, show a message
+    default:
+      content = (
+        <div>Something went wrong in a way that wasn't planned for</div>
+      );
   }
 
   return <Center>{content}</Center>;
