ca74d029d9bd25944b0407650a5296c16848c493
oenu <51684443+oenu@users.noreply.github.com>
2022-09-20 19:52:05 -0400

    init

--- criticalConsent/src/pages/register/Register.tsx
+++ criticalConsent/src/pages/register/Register.tsx
@@ -1,7 +1,22 @@
+import { Container, Center, Card, Stack, Button, Text } from "@mantine/core";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <Container>
+      <Center style={{ width: "100%", height: "60vh" }}>
+        <Card>
+          <Stack align={"center"}>
+            <Text>No Survey found</Text>
+            <Button component={Link} to="/register">
+              Create new survey
+            </Button>
+          </Stack>
+        </Card>
+      </Center>
+    </Container>
+  );
 }
 
 export default Register;
