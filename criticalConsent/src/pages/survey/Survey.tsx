b84a99d3fa05954884daa5ac639f442eb162b3d7
oenu <51684443+oenu@users.noreply.github.com>
2022-09-20 19:07:15 -0400

    moved to own page

---
+++ criticalConsent/src/pages/survey/Survey.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+// Features
+import QuestionList from "../../features/question/QuestionList";
+
+export default function Survey() {
+  return <QuestionList />;
+}
