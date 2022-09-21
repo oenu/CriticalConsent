ad3d0e37975d7e06db2fcc0b54c8cc56bf7c7c23
oenu <51684443+oenu@users.noreply.github.com>
2022-09-21 13:21:45 -0400

    added adult content toggles

--- criticalConsent/src/features/app/appSlice.ts
+++ criticalConsent/src/features/app/appSlice.ts
@@ -1,14 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../redux/store";
 
 export interface AppState {
   darkMode: boolean;
   burgerMenuOpen: boolean;
+  showAdultContent: boolean;
+  acceptAdultDisclaimer: boolean;
 }
 
 const initialState: AppState = {
   darkMode: false,
   burgerMenuOpen: false,
+  showAdultContent: false,
+  acceptAdultDisclaimer: false,
 };
 
 const appSlice = createSlice({
@@ -27,17 +31,40 @@ const appSlice = createSlice({
     closeBurgerMenu: (state) => {
       state.burgerMenuOpen = false;
     },
+    // Toggle whether to show adult content in survey
+    setShowAdultContent: (state, action: PayloadAction<boolean>) => {
+      state.showAdultContent = action.payload;
+    },
+    // Toggle whether survey creator has accepted adult disclaimer
+    setAcceptAdultContent: (state, action: PayloadAction<boolean>) => {
+      state.acceptAdultDisclaimer = action.payload;
+    },
   },
 });
 
-// Export values for selectors
+// Export whether dark mode is enabled
 export const selectDarkMode = (state: RootState) => state.app.darkMode;
+
+// Export whether the burger menu is open
 export const selectBurgerMenuOpen = (state: RootState) =>
   state.app.burgerMenuOpen;
 
+// Export whether to show adult content
+export const selectShowAdultContent = (state: RootState) =>
+  state.app.showAdultContent;
+
+// Export whether the survey creator has accepted adult content disclaimer
+export const selectAcceptAdultDisclaimer = (state: RootState) =>
+  state.app.acceptAdultDisclaimer;
+
 // Export reducer actions
-export const { toggleDarkMode, toggleBurgerMenu, closeBurgerMenu } =
-  appSlice.actions;
+export const {
+  toggleDarkMode,
+  toggleBurgerMenu,
+  closeBurgerMenu,
+  setAcceptAdultContent,
+  setShowAdultContent,
+} = appSlice.actions;
 
 // Export the reducer
 export default appSlice.reducer;
