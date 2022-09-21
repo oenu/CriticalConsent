8f77a280776c807e324ee4c44c8af4be7a8aae03
oenu <51684443+oenu@users.noreply.github.com>
2022-09-21 18:33:27 -0400

    added aysnc thunk to group slice

--- criticalConsent/src/features/group/groupSlice.ts
+++ criticalConsent/src/features/group/groupSlice.ts
@@ -1,16 +1,21 @@
+import { GroupType } from "./../../types.d";
 import { supabase } from "./../../utils/supabaseClient";
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export interface GroupState {
   id: number | null;
   name: string | null;
   nsfw: boolean | null;
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
 }
 
 const initialState: GroupState = {
   id: null,
   name: null,
   nsfw: null,
+  status: "idle",
+  error: null,
 };
 
 const groupSlice = createSlice({
@@ -28,12 +33,46 @@ const groupSlice = createSlice({
     // Register a new group in the database
     registerGroup: (state, action) => {
       console.debug("registerGroup", action.payload);
-      state.id = action.payload.id;
-      state.name = action.payload.name;
-      state.nsfw = action.payload.nsfw;
     },
   },
+  extraReducers(builder) {
+    builder.addCase(fetchGroupByIdAsync.pending, (state, action) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchGroupByIdAsync.fulfilled, (state, action) => {
+      state.status = "succeeded";
+    });
+    builder.addCase(fetchGroupByIdAsync.rejected, (state, action) => {
+      state.status = "failed";
+      console.debug("fetchGroupByIdAsync.rejected", action.error);
+      state.error = action.error.message || "Something went wrong";
+    });
+  },
 });
+
+// Fetch group
+export const fetchGroupByIdAsync = createAsyncThunk(
+  "group/fetchGroupByIdAsync",
+  async (group_id: number): Promise<GroupType[]> => {
+    try {
+      console.debug("fetching group");
+      const { data, error } = await supabase
+        .from("groups")
+        .select("*")
+        .eq("id", group_id);
+      if (error) {
+        console.warn("error fetching group", error);
+        throw error;
+      } else {
+        console.debug("fetched group", data);
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
 
 // Export reducer actions
 export const { registerGroup, setGroup } = groupSlice.actions;
