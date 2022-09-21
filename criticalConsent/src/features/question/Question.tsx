82056fa91ce3e4072d68d389f5e553dbe512045f
oenu <51684443+oenu@users.noreply.github.com>
2022-09-21 16:52:30 -0400

    renamed sub question types

--- criticalConsent/src/features/question/Question.tsx
+++ criticalConsent/src/features/question/Question.tsx
@@ -4,8 +4,9 @@ import { useMediaQuery } from "@mantine/hooks";
 import { QuestionType } from "../../types";
 
 // Import question subtypes
-import SelectQuestion from "./subtypes/SelectQuestion";
-import ToggleQuestion from "./subtypes/ToggleQuestion";
+import Select from "./subtypes/Select";
+import Toggle from "./subtypes/Toggle";
+import CustomResponse from "./subtypes/CustomResponse";
 
 function Question({ question }: { question: QuestionType }) {
   // Detect if the screen is small
@@ -15,13 +16,14 @@ function Question({ question }: { question: QuestionType }) {
   let content = null;
   switch (question.type) {
     case "select":
-      content = <SelectQuestion question={question} mobileView={mobileView} />;
+      content = <Select question={question} mobileView={mobileView} />;
       break;
     case "toggle":
-      content = <ToggleQuestion question={question} mobileView={mobileView} />;
+      content = <Toggle question={question} mobileView={mobileView} />;
       break;
     case "custom_response":
-      content = <div>Custom Question Type</div>;
+      content = <CustomResponse question={question} mobileView={mobileView} />;
+
       break;
     default:
       content = <Text>Unspecified question type id:{question.id} </Text>;
