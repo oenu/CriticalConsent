9c795ce6c2412dad7f36ccc4b125d8f11fb15235
oenu <51684443+oenu@users.noreply.github.com>
2022-09-21 16:39:17 -0400

    added question types and categories

--- criticalConsent/src/types.d.ts
+++ criticalConsent/src/types.d.ts
@@ -1,8 +1,10 @@
 // Type definitions for criticalConsent
 
-// Type for questions from the database
+// Type for questions from the database extended with answered and selected properties
 export interface QuestionType {
   id: number;
+  type: "toggle" | "select" | "custom_response";
+  title: string;
   description: string;
   example_low: string;
   select_low: boolean;
@@ -12,7 +14,9 @@ export interface QuestionType {
   select_high: boolean;
   answered: boolean;
   nsfw: boolean;
-  category: string;
+  category: "sexual" | "graphic" | "offensive" | "phobic" | null;
+  custom_response: string;
+  opt_in: boolean;
 }
 
 // Tye for the group identifier from the database
