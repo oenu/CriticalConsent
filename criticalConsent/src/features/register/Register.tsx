a7a53ab3e720a30e6c272338e6ee6efa42edf30d
oenu <51684443+oenu@users.noreply.github.com>
2022-09-22 23:11:23 -0400

    reconfigured registration screen

--- criticalConsent/src/features/register/Register.tsx
+++ criticalConsent/src/features/register/Register.tsx
@@ -1,9 +1,11 @@
 import {
+  Alert,
   Button,
   Card,
   Center,
   Container,
   Divider,
+  Overlay,
   Stack,
   Switch,
   Text,
@@ -24,6 +26,7 @@ import {
   setGroupPassword,
   setPasswordProtected,
   setPasswordWarning,
+  setMatureContent,
 } from "./registerSlice";
 
 function Register() {
@@ -46,171 +49,181 @@ function Register() {
 
   return (
     <Container>
-      <Center style={{ width: "100%" }}>
-        <Card withBorder>
+      <Card withBorder style={{ width: "100%" }} mb={"md"}>
+        <Stack>
+          <Title order={2}>Group Information</Title>
+          <Divider />
+          <TextInput
+            label="Group Name"
+            placeholder="Consent Champions"
+            withAsterisk
+            required
+            defaultValue={group_name || ""}
+            onBlur={(e) => {
+              dispatch(setGroupName(e.currentTarget.value));
+              if (e.currentTarget.value === "") {
+                dispatch(setGroupNameWarning(true));
+              } else {
+                dispatch(setGroupNameWarning(false));
+              }
+            }}
+            error={group_name_warning ? "Please enter a name" : ""}
+          />
+          <TextInput
+            label="Group Passcode"
+            placeholder="password123"
+            disabled={!password_protected}
+            withAsterisk={password_protected}
+            required={password_protected}
+            onBlur={(e) => {
+              dispatch(setGroupPassword(e.currentTarget.value));
+              if (password_protected && e.currentTarget.value === "") {
+                dispatch(setPasswordWarning(true));
+              } else {
+                dispatch(setPasswordWarning(false));
+              }
+            }}
+            error={password_warning ? "Please enter a passcode" : ""}
+          />
+          <Switch
+            label="Require Passcode"
+            checked={password_protected}
+            onChange={(e) => {
+              dispatch(setPasswordProtected(e.currentTarget.checked));
+              if (!e.currentTarget.checked && password_warning) {
+                dispatch(setPasswordWarning(false));
+              }
+            }}
+          />
+        </Stack>
+      </Card>
+
+      <Card withBorder style={{ width: "100%" }} my="md">
+        {/* Card to explain mature content and enable it */}
+        <Stack>
+          <Title order={2}>Mature Content</Title>
+          <Divider />
+          <Text italic>
+            By default, mature content is disabled for surveys. This means that
+            players wont be asked whether they are comfortable with topics
+            below. If you wish to include these themes in your game you can
+            enable Mature Content questions for each category, but be aware that
+            this may make your surveys less accessible to some players.
+          </Text>
+          <Alert title="Remember" color={"orange"}>
+            You must respect your player's responses. Anything excluded by a
+            player must be excluded from the game.
+          </Alert>
+          <Switch
+            checked={showMatureContent || false}
+            onChange={(e) => {
+              console.log("Mature Content", e.target.checked);
+              dispatch(setMatureContent(e.target.checked));
+            }}
+            value="mature"
+            label="Enable Mature Content Questions"
+          />
+        </Stack>
+      </Card>
+
+      <Card withBorder my="md">
+        {/* Disable section if mature content is not allowed */}
+        {showMatureContent ? null : <Overlay zIndex={5} />}
+        <Stack>
+          {/* Content toggles */}
           <Stack>
-            <Title order={2}>Group Information</Title>
-            <TextInput
-              label="Group Name"
-              placeholder="Consent Champions"
-              withAsterisk
-              required
-              defaultValue={group_name || ""}
-              onBlur={(e) => {
-                dispatch(setGroupName(e.currentTarget.value));
-                if (e.currentTarget.value === "") {
-                  dispatch(setGroupNameWarning(true));
-                } else {
-                  dispatch(setGroupNameWarning(false));
-                }
+            <Title order={3}>Graphic Content</Title>
+            <Text size={"xs"} italic>
+              Graphic content questions address depictions of extreme violence,
+              bodily damage sustained in combat or by torture, themes of self
+              harm, violence against animals, child exploitation, cannibalism,
+              and virulent diseases.
+            </Text>
+            <Switch
+              checked={graphic_content}
+              onChange={(e) => {
+                console.log("Graphic Content", e.target.checked);
+                dispatch(
+                  setCategories({
+                    category: "graphic",
+                    value: e.target.checked,
+                  })
+                );
               }}
-              error={group_name_warning ? "Please enter a name" : ""}
+              value="graphic"
+              label="Include Graphic Content Questions"
             />
-            <>
-              <Switch
-                label="Require Passcode"
-                checked={password_protected}
-                onChange={(e) => {
-                  dispatch(setPasswordProtected(e.currentTarget.checked));
-                  if (!e.currentTarget.checked && password_warning) {
-                    dispatch(setPasswordWarning(false));
-                  }
-                }}
-              />
-              <TextInput
-                label="Group Passcode"
-                placeholder="password123"
-                disabled={!password_protected}
-                withAsterisk={password_protected}
-                required={password_protected}
-                onBlur={(e) => {
-                  dispatch(setGroupPassword(e.currentTarget.value));
-                  if (password_protected && e.currentTarget.value === "") {
-                    dispatch(setPasswordWarning(true));
-                  } else {
-                    dispatch(setPasswordWarning(false));
-                  }
-                }}
-                error={password_warning ? "Please enter a passcode" : ""}
-              />
-            </>
+            <Divider />
+            <Title order={3}>Sexual Content</Title>
+            <Text size={"xs"} italic>
+              Sexual content questions address depictions of sexual intercourse,
+              nudity, prostitution/sex work, sexualized descriptions of
+              characters, and descriptions of sexual assault.
+            </Text>
 
-            {/* Content toggles */}
-            <Stack>
-              {/* <Text>
-                If a player indicates that they uncomfortable with included
-                content it should not be used in games
-              </Text> */}
-              <Divider />
-              <Title order={2}>Mature Content</Title>
-              <Switch
-                checked={showMatureContent || false}
-                onChange={(e) => {
-                  console.log("Mature Content", e.target.checked);
-                  dispatch(
-                    setCategories({
-                      category: "graphic",
-                      value: e.target.checked,
-                    })
-                  );
-                }}
-                value="mature"
-                label="Enable Mature Content Questions"
-              />
-              {/* <Text>Select topics to include in your survey</Text> */}
-              <Title order={3}>Graphic Content</Title>
-              <Text size={"xs"} italic>
-                Graphic content questions address depictions of extreme
-                violence, bodily damage sustained in combat or by torture,
-                themes of self harm, violence against animals, child
-                exploitation, cannibalism, and virulent diseases.
-              </Text>
-              <Switch
-                checked={graphic_content}
-                onChange={(e) => {
-                  console.log("Graphic Content", e.target.checked);
-                  dispatch(
-                    setCategories({
-                      category: "graphic",
-                      value: e.target.checked,
-                    })
-                  );
-                }}
-                value="graphic"
-                label="Include Graphic Content Questions"
-              />
-              <Divider />
-              <Title order={3}>Sexual Content</Title>
-              <Text size={"xs"} italic>
-                Sexual content questions address depictions of sexual
-                intercourse, nudity, prostitution/sex work, sexualized
-                descriptions of characters, and descriptions of sexual assault.
-              </Text>
+            <Switch
+              checked={sexual_content}
+              onChange={(e) => {
+                console.log("Sexual Content", e.target.checked);
+                dispatch(
+                  setCategories({
+                    category: "sexual",
+                    value: e.target.checked,
+                  })
+                );
+              }}
+              value="sexual"
+              label="Include Sexual Content Questions"
+            />
+            <Divider />
+            <Title order={3}>Potentially Offensive Content</Title>
+            <Text size={"xs"} italic>
+              Potentially Offensive content questions address depictions of
+              racism, homophobia, transphobia, misogyny, extreme poverty,
+              slavery. The questions offer distinctions between wider themes in
+              a fantasy world and content directed at/from players.
+            </Text>
+            <Switch
+              checked={offensive_content}
+              onChange={(e) => {
+                console.log("Offensive Content", e.target.checked);
+                dispatch(
+                  setCategories({
+                    category: "offensive",
+                    value: e.target.checked,
+                  })
+                );
+              }}
+              value="offensive"
+              label="Include Potentially Offensive Content Questions"
+            />
 
-              <Switch
-                checked={sexual_content}
-                onChange={(e) => {
-                  console.log("Sexual Content", e.target.checked);
-                  dispatch(
-                    setCategories({
-                      category: "sexual",
-                      value: e.target.checked,
-                    })
-                  );
-                }}
-                value="sexual"
-                label="Include Sexual Content Questions"
-              />
-              <Divider />
-              <Title order={3}>Potentially Offensive Content</Title>
-              <Text size={"xs"} italic>
-                Potentially Offensive content questions address depictions of
-                racism, homophobia, transphobia, misogyny, extreme poverty,
-                slavery. The questions offer distinctions between wider themes
-                in a fantasy world and content directed at/from players.
-              </Text>
-              <Switch
-                checked={offensive_content}
-                onChange={(e) => {
-                  console.log("Offensive Content", e.target.checked);
-                  dispatch(
-                    setCategories({
-                      category: "offensive",
-                      value: e.target.checked,
-                    })
-                  );
-                }}
-                value="offensive"
-                label="Include Potentially Offensive Content Questions"
-              />
+            <Divider />
+            <Title order={3}>Common Phobic Content</Title>
+            <Text size={"xs"} italic>
+              Common Phobic Content questions address depictions of spiders,
+              snakes, heights, and other common phobias.
+            </Text>
 
-              <Divider />
-              <Title order={3}>Common Phobic Content</Title>
-              <Text size={"xs"} italic>
-                Common Phobic Content questions address depictions of spiders,
-                snakes, heights, and other common phobias.
-              </Text>
-
-              <Switch
-                checked={phobic_content}
-                onChange={(e) => {
-                  console.log("Phobic Content", e.target.checked);
-                  dispatch(
-                    setCategories({
-                      category: "phobic",
-                      value: e.target.checked,
-                    })
-                  );
-                }}
-                value="phobic"
-                label="Include Common Phobic Content Questions"
-              />
-
-              <Divider />
-            </Stack>
-            <Button>Create Survey</Button>
+            <Switch
+              checked={phobic_content}
+              onChange={(e) => {
+                console.log("Phobic Content", e.target.checked);
+                dispatch(
+                  setCategories({
+                    category: "phobic",
+                    value: e.target.checked,
+                  })
+                );
+              }}
+              value="phobic"
+              label="Include Common Phobic Content Questions"
+            />
           </Stack>
-        </Card>
+        </Stack>
+      </Card>
+      <Center>
+        <Button>Create Survey</Button>
       </Center>
     </Container>
   );
