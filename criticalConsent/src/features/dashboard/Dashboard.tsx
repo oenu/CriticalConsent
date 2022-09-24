67b4069f640ec9feaf1494152d8a4370dc932a1d
oenu <51684443+oenu@users.noreply.github.com>
2022-09-24 11:24:29 -0400

    added qr code modal

--- criticalConsent/src/features/dashboard/Dashboard.tsx
+++ criticalConsent/src/features/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardSection,
@@ -7,6 +8,7 @@ import {
   Divider,
   Grid,
   Group,
+  Modal,
   Overlay,
   Stack,
   ThemeIcon,
@@ -27,8 +29,11 @@ import {
   clearGroup,
   selectGroupCategories,
   selectGroupName,
+  selectGroupId,
 } from "../group/groupSlice";
-import { clearSurvey } from "../survey/surveySlice";
+import { clearSurvey, selectShareCode } from "../survey/surveySlice";
+import QRCode from "react-qr-code";
+import { selectQrModalOpen, setQrModalOpen } from "./dashboardSlice";
 
 // Dashboard page showing the current survey, the responses, a link to the survey, a qr code
 
@@ -42,6 +47,9 @@ function Dashboard() {
   // Get group information
   const group_name = useAppSelector(selectGroupName);
 
+  // Get group id for qr code
+  const group_id = useAppSelector(selectGroupId);
+
   // Get group categories
   const categories = useAppSelector(selectGroupCategories);
 
@@ -51,11 +59,58 @@ function Dashboard() {
   // Whether in wide view
   const isWide = useMediaQuery("(min-width: 1200px)");
 
+  // Share code for the group
+  const shareCode = useAppSelector(selectShareCode);
+
+  // Monitor whether the store has reset, if so navigate to the survey page
+  React.useEffect(() => {
+    if (group_name === null) {
+      navigate("/survey");
+    }
+  }, [group_name, categories, navigate]);
+
+  // QR code modal
+  const qr_modal_open = useAppSelector(selectQrModalOpen);
+
+  const qrModal = (
+    <Modal
+      size="sm"
+      trapFocus
+      fullScreen={isMobile}
+      centered
+      overlayOpacity={0.9}
+      overlayBlur={5}
+      opened={qr_modal_open}
+      onClose={() => {
+        dispatch(setQrModalOpen(false));
+      }}
+    >
+      <Stack>
+        <Title align="center" order={2}>
+          {group_name}
+        </Title>
+
+        <Center>
+          <QRCode value={`http://127.0.0.1:5173/survey/${group_id}`} />
+        </Center>
+        <Title align="center" order={1}>
+          OR
+        </Title>
+        <Title align="center" order={3}>
+          http://URL HERE /survey and enter the share code:
+        </Title>
+        <Card>
+          <Title align="center" order={2}>
+            {shareCode}
+          </Title>
+        </Card>
+      </Stack>
+    </Modal>
+  );
+
   // Indicator of how many responses have been submitted
 
   // Indicator of how many responses are required
-
-  // QR code for the survey
 
   // Email link for the survey
 
@@ -65,6 +120,7 @@ function Dashboard() {
 
   content = (
     <Container>
+      {qrModal}
       <Stack style={isWide ? { maxWidth: 1200 } : {}}>
         <Group align={"center"}>
           <Title order={2}>{group_name}</Title>
@@ -88,7 +144,7 @@ function Dashboard() {
             <Stack>
               <Card withBorder>
                 <Stack>
-                  <Title order={3}>Categories</Title>
+                  <Title order={3}>Question Categories</Title>
                   <Divider />
                   <Stack>
                     <Group>
@@ -98,25 +154,37 @@ function Dashboard() {
                       <Title order={5}>General</Title>
                     </Group>
                     <Group>
-                      <ThemeIcon variant="filled" color="red">
+                      <ThemeIcon
+                        variant="filled"
+                        color={categories["phobic"] ? "green" : "red"}
+                      >
                         <IconSpider />
                       </ThemeIcon>
                       <Title order={5}>Phobic</Title>
                     </Group>
                     <Group>
-                      <ThemeIcon variant="filled" color="orange">
+                      <ThemeIcon
+                        variant="filled"
+                        color={categories["graphic"] ? "green" : "red"}
+                      >
                         <IconTank />
                       </ThemeIcon>
                       <Title order={5}>Graphic</Title>
                     </Group>
                     <Group>
-                      <ThemeIcon variant="filled" color="orange">
+                      <ThemeIcon
+                        variant="filled"
+                        color={categories["sexual"] ? "green" : "red"}
+                      >
                         <IconSausage />
                       </ThemeIcon>
                       <Title order={5}>Sexual</Title>
                     </Group>
                     <Group>
-                      <ThemeIcon variant="filled" color="green">
+                      <ThemeIcon
+                        variant="filled"
+                        color={categories["offensive"] ? "green" : "red"}
+                      >
                         <IconMoodCry />
                       </ThemeIcon>
                       <Title order={5}>Offensive</Title>
@@ -125,12 +193,18 @@ function Dashboard() {
                 </Stack>
               </Card>
               <Card withBorder>
-                <CardSection>
-                  <Title order={3} m="md">
-                    QR Code
-                  </Title>
-                </CardSection>
-                <CardSection></CardSection>
+                <Stack>
+                  <Title order={3}>QR Code</Title>
+                  <Button
+                    onClick={() => {
+                      dispatch(setQrModalOpen(true));
+                    }}
+                  />
+
+                  <Stack>
+                    <Divider />
+                  </Stack>
+                </Stack>
               </Card>
             </Stack>
           </Grid.Col>
